4c535685295eda6aabd0895127976d44a2a6766c
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2024-02-10 07:18:37 +0000

    コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :users
+  
   root to: 'homes#top'
+  
+  # 管理者用
+  devise_for :admin, skip: [:registrations ], controllers: {
+    sessions: "admin/sessions"
+  }
+    
+  # 顧客用
+  devise_for :user, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+  
+  scope module: :user do
+    get 'users/mypage' => 'users#show'
+  end
+
 end
