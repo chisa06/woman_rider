f9f6a11e2a6e8b05d6a3c3ad53a1665355dc7d1f
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2024-03-14 13:52:35 +0000

    DM処理修正

--- app/controllers/user/direct_messages_controller.rb
+++ app/controllers/user/direct_messages_controller.rb
@@ -1,2 +1,17 @@
 class User::DirectMessagesController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
+  
+  def create
+    if params[:direct_message].present? && Entry.where(user_id: current_user.id, room_id: params[:direct_message][:room_id]).present?
+      @direct_message = DirectMessage.create(direct_message_params.merge(user_id: current_user.id))
+      redirect_to "/rooms/#{params[:direct_message][:room_id]}"
+    else
+      # エラー処理など
+    end
+  end
+  
+  def direct_message_params
+    params.require(:direct_message).permit(:message, :user_id, :room_id)
+  end
+
 end
